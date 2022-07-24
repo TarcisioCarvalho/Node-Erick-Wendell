df9a548a7cfb9989058bec83fa3955d65424b9db
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-07-23 22:21:34 -0300

    Funcoes de callback

---
+++ aula-02/index.js
@@ -0,0 +1,17 @@
+function obterUsuario(){
+    setTimeout(()=>{
+        return{
+            id:1,
+            nome:'Aladim',
+            dataNascimento:new Date()
+        }
+    },1000)
+}
+
+function obterTelefone(idUsuario){
+
+}
+
+function obterEndereco(idUsuario){
+    
+}
